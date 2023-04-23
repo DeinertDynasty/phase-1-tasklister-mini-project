6a6e2b7065c73baa158e0ce4efc442316eaf0335
Phoenix Deinert <phoenix.deinert@gmail.com>
2023-04-22 22:09:01 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const createTaskForm = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  createTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+    const taskDescriptionInput = document.getElementById('new-task-description');
+    const taskDescription = taskDescriptionInput.value;
+
+    if (taskDescription) {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskDescription;
+      taskList.appendChild(newTask);
+
+      // Clear the input field after adding the task
+      taskDescriptionInput.value = '';
+    } else {
+      alert('Please enter a task description');
+    }
+  });
 });
